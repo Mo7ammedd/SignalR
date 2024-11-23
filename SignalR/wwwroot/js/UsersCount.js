46e07ab91af07d7d8acd08e8f9fadbc03c963100
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-11-23 11:34:08 +0200

    estaplishing the signalR

---
+++ SignalR/wwwroot/js/UsersCount.js
@@ -0,0 +1,22 @@
+﻿// Create a connection to the hub
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hub/userCount").build();
+
+// Connect  to method in the hub invokes aka receives the message
+
+connectionUserCount.on("UpdateTotalView",(value)=>{
+    var newCountSpan = document.getElementById("TotalViewsCounter");
+    newCountSpan.innerHTML = value.toString();
+})
+// invoke hub methods
+
+function newWindowLoadedOnClientSide(){
+    connectionUserCount.invoke("NewWindowLoaded");
+}
+
+// Start the connection
+connectionUserCount.start().then(function(){
+    console.log("Connected to UserCount Hub");
+    newWindowLoadedOnClientSide();
+}).catch(function(err){
+    return console.error(err.toString());
+});
