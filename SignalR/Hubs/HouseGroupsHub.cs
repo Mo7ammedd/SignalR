8ac4751b9f04c2e304d20c5565466b2b75f99c99
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-11-23 15:12:52 +0200

    add HouseGroupsHub

---
+++ SignalR/Hubs/HouseGroupsHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs;
+
+public class HouseGroupsHub : Hub
+{
+    private static List<string> GroupsJoined { get;set; } = new List<string>();
+    
+    public async Task JoinHouse(string houseName)
+    {
+        if (!GroupsJoined.Contains( Context.ConnectionId+"_"+houseName))
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, houseName);
+            GroupsJoined.Add(Context.ConnectionId+"_"+houseName);
+        }
+       
+    }
+    public async Task LeaveHouse(string houseName)
+    {
+        if (GroupsJoined.Contains( Context.ConnectionId+"_"+houseName))
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, houseName);
+            GroupsJoined.Remove(Context.ConnectionId+"_"+houseName);
+        }
+    }
+    
+}
