dbfb182f88456d1ba76fa7f0de9cc9002caf9abd
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-11-23 11:10:48 +0200

    add libman install @microsoft/signalr --provider unpkg --destination wwwroot/lib/signalr

---
+++ SignalR/Hubs/UserHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs;
+
+public class UserHub : Hub
+{
+    public static int ViewersCount { get; set; }=0;
+    public async Task NewWindowLoaded()
+    {
+        ViewersCount++;
+        await Clients.All.SendAsync("UpdateTotalView", ViewersCount);
+    }
+}
