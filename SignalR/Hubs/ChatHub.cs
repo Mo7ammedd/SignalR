02b23d541affb51720bf1101bd89cb0ba1e73445
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-11-24 15:12:47 +0200

    added OnConnectedAsync

--- SignalR/Hubs/ChatHub.cs
+++ SignalR/Hubs/ChatHub.cs
@@ -1,9 +1,65 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Data;
 
 namespace SignalR.Hubs;
 
 public class ChatHub : Hub
 {
+    private readonly ApplicationDbContext _db;
+    public ChatHub(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        var UserId = Context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (!String.IsNullOrEmpty(UserId))
+        {
+            var userName = _db.Users.FirstOrDefault(u => u.Id == UserId).UserName;
+            Clients.Users(HubConnections.OnlineUsers()).SendAsync("ReceiveUserConnected", UserId, userName);    
+            HubConnections.AddUserConnection(UserId, Context.ConnectionId);
+        }
+        return base.OnConnectedAsync();
+    }
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        var UserId = Context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (HubConnections.HasUserConnection(UserId, Context.ConnectionId))
+        {
+            var UserConnections = HubConnections.Users[UserId];
+            UserConnections.Remove(Context.ConnectionId);
+                
+            HubConnections.Users.Remove(UserId);
+            if(UserConnections.Any())
+                HubConnections.Users.Add(UserId, UserConnections);
+        }
+        if (!String.IsNullOrEmpty(UserId))
+        {
+            var userName = _db.Users.FirstOrDefault(u => u.Id == UserId).UserName;
+            Clients.Users(HubConnections.OnlineUsers()).SendAsync("ReceiveUserDisconnected", UserId, userName);
+            HubConnections.AddUserConnection(UserId, Context.ConnectionId);
+        }
+        return base.OnDisconnectedAsync(exception);
+    }
     
+
+    //public async Task SendMessageToAll(string user, string message)
+    //{
+    //    await Clients.All.SendAsync("MessageReceived", user, message);
+    //}
+    //[Authorize]
+    //public async Task SendMessageToReceiver(string sender, string receiver, string message)
+    //{
+    //    var userId = _db.Users.FirstOrDefault(u => u.Email.ToLower() == receiver.ToLower()).Id;
+
+    //    if (!string.IsNullOrEmpty(userId))
+    //    {
+    //        await Clients.User(userId).SendAsync("MessageReceived", sender, message);
+    //    }
+
+    //}
+
+} 
     
-}
---
+++ SignalR/wwwroot/js/Chat.js
@@ -0,0 +1,32 @@
+﻿
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub/chat")
+    .withAutomaticReconnect([0, 1000, 5000, null])
+    .build();
+
+
+connection.on("ReceiveUserConnected", function (userId, userName) {
+
+    addMessage(`${userName} has openned a connection`);
+
+});
+
+connection.on("ReceiveUserDisconnected", function (userId, userName) {
+
+    addMessage(`${userName} has closed a connection`);
+
+});
+
+
+
+function addMessage(msg) {
+    if (msg == null && msg == '') {
+        return;
+    }
+    let ui = document.getElementById('messagesList');
+    let li = document.createElement("li");
+    li.innerHTML = msg;
+    ui.appendChild(li);
+}
+
+connection.start();
